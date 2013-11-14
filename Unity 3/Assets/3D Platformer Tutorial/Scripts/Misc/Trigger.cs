0fd9b25a1f732932cae3c977d76a0ba3191795a2
alfred316 <ashaker@kent.edu>
2013-11-14 18:06:55 -0500

    work in progress

---
+++ Unity 3/Assets/3D Platformer Tutorial/Scripts/Misc/Trigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	float rotationVal;
+	float rotation;
+	bool moving;
+	
+	// Use this for initialization
+	void Start () {
+	   rotationVal = 0.25f;
+	   rotation = rotationVal;
+	   moving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!moving) return;
+			transform.Rotate(0,rotation,0,Space.Self);
+		  if (transform.rotation.eulerAngles.y > 89.9){
+			//transform.rotation.y = 90.0;
+		    rotation = -rotationVal;
+		  }
+		  else if (transform.rotation.eulerAngles.y < 0.1){
+			//transform.rotation.y =eulerAngles. 0.0;
+			 rotation = rotationVal;
+		  }	
+	}
+	
+
+
+	void OnTriggerEnter(Collider other) {
+	   if (moving)
+			moving = false;
+		else
+			moving = true;
+
+	}	
+	void OnTriggerExit(Collider other) {
+ 	   moving = true;
+
+	
+	}
+}
---
+++ Unity 3/Assets/doorTrigger.js
@@ -0,0 +1,73 @@
+﻿var open : boolean;
+var trigger : boolean;
+var blocked : boolean;
+
+var Smooth : float = 1.5;
+var OpenAngle : float = 110.0;
+var CloseAngle : float = 0.0;
+
+var timer : int = 0;
+
+
+
+function OnTriggerEnter (col : Collider) 
+{
+var controller : ThirdPersonController = col.GetComponent(ThirdPersonController);
+if (controller != null)
+{
+if (!open)
+trigger = true;
+}
+
+}
+
+function OnTriggerStay (col : Collider) 
+{
+var controller : ThirdPersonController = col.GetComponent(ThirdPersonController);
+if (controller != null)
+{
+blocked = true;
+
+}
+}
+
+function OnTriggerExit (col : Collider) 
+{
+var controller : ThirdPersonController = col.GetComponent(ThirdPersonController);
+if (controller != null)
+{
+
+blocked = false;
+
+}
+}
+
+
+function Start () {}
+
+function Update () {
+
+if(trigger && !blocked && !open){
+        var DoorOpen = Quaternion.Euler(0, OpenAngle, 0);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, DoorOpen, Time.deltaTime * Smooth);
+        
+
+       timer++;
+       if(timer >= 110) {
+       	open = true;
+       	trigger = false;
+       }
+} 
+
+
+if(!trigger && !blocked && open){
+       var DoorClosed = Quaternion.Euler(0, CloseAngle, 0);
+       transform.localRotation = Quaternion.Slerp(transform.localRotation, DoorClosed, Time.deltaTime * Smooth);
+
+timer--;
+if(timer <= 15)
+open = false;
+
+}
+
+}
